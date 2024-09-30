2fe0afec1e899c9e889c68b33e24adad169aad45
Rani Bungay <ranibungay@gmail.com>
2024-09-30 18:25:34 -0400

    changed everything to type module

--- models/user.js
+++ models/user.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema({
   username: {
@@ -11,6 +11,7 @@ const userSchema = mongoose.Schema({
   },
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
-module.exports = User;
+export default User;
+// module.exports = User;
--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-const dotenv = require('dotenv');
+import dotenv from "dotenv";
 dotenv.config();
-const express = require('express');
+import express from "express";
 const app = express();
-const mongoose = require('mongoose');
-const methodOverride = require('method-override');
-const morgan = require('morgan');
-const session = require('express-session');
+import mongoose from "mongoose";
+import methodOverride from "method-override";
+import morgan from "morgan";
+import session from "express-session";
 
-const authController = require('./controllers/auth.js');
+import authController from "./controllers/auth.js";
 
-const port = process.env.PORT ? process.env.PORT : '3000';
+const port = process.env.PORT ? process.env.PORT : "3000";
 
 mongoose.connect(process.env.MONGODB_URI);
 
-mongoose.connection.on('connected', () => {
+mongoose.connection.on("connected", () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
 app.use(express.urlencoded({ extended: false }));
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 // app.use(morgan('dev'));
 app.use(
   session({
@@ -28,21 +28,22 @@ app.use(
   })
 );
 
-app.get('/', (req, res) => {
-  res.render('index.ejs', {
+app.get("/", (req, res) => {
+  res.render("index.ejs", {
     user: req.session.user,
   });
 });
 
-app.get('/vip-lounge', (req, res) => {
+app.get("/vip-lounge", (req, res) => {
   if (req.session.user) {
     res.send(`Welcome to the party ${req.session.user.username}.`);
   } else {
-    res.send('Sorry, no guests allowed.');
+    res.send("Sorry, no guests allowed.");
   }
 });
 
-app.use('/auth', authController);
+app.use("/auth", authController);
+// app.use("/main", mainController);
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
