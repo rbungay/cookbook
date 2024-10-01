8a38b6513915293f30d39aab68fbcf2ae3136566
Rani Bungay <ranibungay@gmail.com>
2024-09-30 22:01:01 -0400

    changed the main ejs

--- controllers/main.js
+++ controllers/main.js
@@ -4,7 +4,6 @@ const router = express.Router();
 import User from "../models/user.js";
 
 router.get("/myCookBook", (req, res) => {
-  console.log("this is going through this route");
   res.render("main.ejs", {
     user: req.session.user,
   });
